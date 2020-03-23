6b4be5f7f634604189329c3a55f03005173288d6
danielbras <carloosdd9550@gmail.com>
2020-03-23 12:13:20 -0300

    removed file

--- src/CustomReturnBegin.js
+++
@@ -1,9 +0,0 @@
-export default function customRemoveBeginning() {
-    if (this.isEmpty()) {
-        throw new Error();
-    } else {
-        let aux = this.head.next;
-        this.head.next = aux.next;
-        return aux.data;
-    }
-}
